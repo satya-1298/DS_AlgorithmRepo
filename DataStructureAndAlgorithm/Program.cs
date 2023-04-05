f230668119ba44ef657e9a1dc6a27b4e4c4e2b4d
unknown <satya-1298>
2023-04-05 18:52:34 +0530

    call method

--- DataStructureAndAlgorithm/Program.cs
+++ DataStructureAndAlgorithm/Program.cs
@@ -4,9 +4,33 @@
     {
         public static void Main(string[] args) 
         {
-            Console.WriteLine("These are Ds and Algorithm Programs");
-            BinarySearchWord binarySearchWord = new BinarySearchWord();
-            binarySearchWord.SearchWord(@"D:\assignments\DS_AlgorithmRepo\DataStructureAndAlgorithm\file.txt");
+            //Console.WriteLine("These are Ds and Algorithm Programs");
+            //BinarySearchWord binarySearchWord = new BinarySearchWord();
+            //binarySearchWord.SearchWord(@"D:\assignments\DS_AlgorithmRepo\DataStructureAndAlgorithm\file.txt");
+            //Console.WriteLine("\n");
+            //Anagram a=new Anagram();
+            //a.Check();
+            //Console.WriteLine("\n");
+            //Bubblesort.Display();
+            //InsertionSort sort = new InsertionSort();
+            //sort.Display();
+            int[] numbers = { 38, 27, 43, 3, 0, 82, 10 };
+            int len = numbers.Length;
+            Console.WriteLine("Before Merge Sort:");
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("After Merge Sort");
+            Merge.SortMethod(numbers, 0, len - 1);
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Read();
         }
     }
+
 }
+    
