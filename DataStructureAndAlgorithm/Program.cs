f26b5eba7159ae2e08134b34111aea129c01c52e
unknown <satya-1298>
2023-04-05 19:54:27 +0530

    Prime Numbers up to a range

--- DataStructureAndAlgorithm/Program.cs
+++ DataStructureAndAlgorithm/Program.cs
@@ -4,9 +4,18 @@
     {
         public static void Main(string[] args) 
         {
-            Console.WriteLine("These are Ds and Algorithm Programs");
-            BinarySearchWord binarySearchWord = new BinarySearchWord();
-            binarySearchWord.SearchWord(@"D:\assignments\DS_AlgorithmRepo\DataStructureAndAlgorithm\file.txt");
+            //Console.WriteLine("These are Ds and Algorithm Programs");
+            //BinarySearchWord binarySearchWord = new BinarySearchWord();
+            //binarySearchWord.SearchWord(@"D:\assignments\DS_AlgorithmRepo\DataStructureAndAlgorithm\file.txt");
+            //Console.WriteLine("\n");
+            //Anagram a = new Anagram();
+            //a.Check();
+            PrimeNumber p = new PrimeNumber();
+            p.Display();
+
+
         }
     }
+
 }
+    
