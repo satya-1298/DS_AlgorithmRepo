8aed681d4263cf6779d9ea47eefc681e439478d4
unknown <satya-1298>
2023-04-05 19:02:36 +0530

    insert values

--- DataStructureAndAlgorithm/Program.cs
+++ DataStructureAndAlgorithm/Program.cs
@@ -4,9 +4,24 @@
     {
         public static void Main(string[] args) 
         {
-            Console.WriteLine("These are Ds and Algorithm Programs");
-            BinarySearchWord binarySearchWord = new BinarySearchWord();
-            binarySearchWord.SearchWord(@"D:\assignments\DS_AlgorithmRepo\DataStructureAndAlgorithm\file.txt");
+            int[] numbers = { 38, 27, 43, 3, 0, 82, 10 };
+            int len = numbers.Length;
+            Console.WriteLine("Before Merge Sort:");
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("After Merge Sort");
+            Merge.SortMethod(numbers, 0, len - 1);
+            foreach (int item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Read();
+
         }
     }
+
 }
+    
